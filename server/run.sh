211a55107c318c5fe590cd2eae7b93a4c4a03886
redundant4u <rafch@naver.com>
2020-11-06 01:49:53 +0900

    [S] Set develop environment container > host(node)

--- server/run.sh
+++ server/run.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
+docker run \
+	-d \
+	--name asd \
+	-e POSTGRES_USER=codi \
+	-e POSTGRES_PASSWORD=codicodi \
+	-e LANG=ko_KR.utf8 \
+	-p 5432:5432 \
+	codi_db:1.0
+
 yarn
+sleep 1
 yarn db:create
 
 if [ $? -eq 0 ];then
@@ -8,4 +18,4 @@ if [ $? -eq 0 ];then
 	yarn db:seed
 fi
 
-exec "$@"
+yarn dev
